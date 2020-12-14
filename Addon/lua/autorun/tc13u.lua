684b73ca51a0ff8df834d49ab3df6ea06d9d3608
Helios <vakkendwarf@gmail.com>
2020-12-14 16:06:12 +0100

    restructure

--- Addon/lua/autorun/tc13u.lua
+++
@@ -1,8 +0,0 @@
-local function AddPlayerModel( name, model )
-
-	list.Set( "PlayerOptionsModel", name, model )
-	player_manager.AddValidModel( name, model )
-
-end
-
-AddPlayerModel( "[TC13u] CT", "models/helios/tc13u/basic/ct.mdl" )
